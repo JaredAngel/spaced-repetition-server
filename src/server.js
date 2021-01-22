98463cb91f799263f79185cee22010a31deaebf6
Jared Angel <jared.aescobedo@gmail.com>
2021-01-22 13:21:12 -0600

    deploy

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
-
+const pg = require('pg')
 const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
-
+pg.defaults.ssl = process.env.NODE_ENV === "production"
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
